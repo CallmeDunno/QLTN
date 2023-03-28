be3c22bc2cefc2f77a16e0576337683964f31c52
Dunno <dungworkit@gmail.com>
2023-03-29 02:14:10 +0700

    Add project files.

---
+++ BTL/Controllers/HostController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BTL.Controllers
+{
+    public class HostController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
