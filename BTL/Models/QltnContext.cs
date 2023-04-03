db8e5839e010c964dd47be13671aac01dfa6e0e7
Quynh Anh <nguyenquynhanh9a3@gmail.com>
2023-04-03 22:20:05 +0700

    QA dag lm

--- BTL/Models/QltnContext.cs
+++ BTL/Models/QltnContext.cs
@@ -41,7 +41,7 @@ public partial class QltnContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Data Source=DUNNO\\SQLEXPRESS;Initial Catalog=QLTN;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False");
+        => optionsBuilder.UseSqlServer("Data Source=LAPTOP-LEDRJD7A\\SQLEXPRESS;Initial Catalog=QLTN;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
