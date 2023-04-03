3d5326688278e893caad1545b68eadac80b3352b
Quynh Anh <nguyenquynhanh9a3@gmail.com>
2023-04-03 23:14:46 +0700

    QA hostcontroller

--- BTL/Controllers/HostController.cs
+++ BTL/Controllers/HostController.cs
@@ -1,17 +1,33 @@
 ﻿using BTL.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using X.PagedList;
 
 namespace BTL.Controllers
 {  
     public class HostController : Controller
     {
         QltnContext db = new QltnContext();
+        private readonly ILogger<HostController> _logger;
 
-        public IActionResult Index()
+        public HostController(ILogger<HostController> logger)
+        {
+            _logger = logger;
+        }
+        public IActionResult Index(int? page)
+        {
+            int pageSize = 6;
+            int pageNumber = page == null || page < 0 ? 1 : page.Value;
+            var lstchunha = db.ChuNhas.AsNoTracking().OrderBy(x => x.MaChuNha).ToList();
+            PagedList<ChuNha> lst = new PagedList<ChuNha>(lstchunha, pageNumber, pageSize);
+            return View(lst);
+        }
+
+       /* public IActionResult Index(int )
         {
             var chunhas = db.ChuNhas.ToList();
             //ViewBag.ChuNhas = chunhas;
             return View(chunhas);
-        }
+        }*/
     }
 }
