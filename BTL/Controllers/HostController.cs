ab16eec75e1a0ebc02acb26a94ec7b819cf182a2
Dunno <dungworkit@gmail.com>
2023-04-04 17:09:46 +0700

    Revert "QA hostcontroller"
    This reverts commit 3d5326688278e893caad1545b68eadac80b3352b.

--- BTL/Controllers/HostController.cs
+++ BTL/Controllers/HostController.cs
@@ -1,33 +1,17 @@
 ﻿using BTL.Models;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using X.PagedList;
 
 namespace BTL.Controllers
 {  
     public class HostController : Controller
     {
         QltnContext db = new QltnContext();
-        private readonly ILogger<HostController> _logger;
 
-        public HostController(ILogger<HostController> logger)
-        {
-            _logger = logger;
-        }
-        public IActionResult Index(int? page)
-        {
-            int pageSize = 6;
-            int pageNumber = page == null || page < 0 ? 1 : page.Value;
-            var lstchunha = db.ChuNhas.AsNoTracking().OrderBy(x => x.MaChuNha).ToList();
-            PagedList<ChuNha> lst = new PagedList<ChuNha>(lstchunha, pageNumber, pageSize);
-            return View(lst);
-        }
-
-       /* public IActionResult Index(int )
+        public IActionResult Index()
         {
             var chunhas = db.ChuNhas.ToList();
             //ViewBag.ChuNhas = chunhas;
             return View(chunhas);
-        }*/
+        }
     }
 }
