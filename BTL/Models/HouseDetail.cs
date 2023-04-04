1eb86e99e82e833c08efd586f307d3aaa061eaa8
Dunno <dungworkit@gmail.com>
2023-04-04 23:56:38 +0700

    Dungx commit image 800x800 With HouseDetail

---
+++ BTL/Models/HouseDetail.cs
@@ -0,0 +1,20 @@
+﻿namespace BTL.Models
+{
+    public class HouseDetail
+    {
+
+        public ThongTinNha thongTinNha { get; set; }
+
+        public ChuNha chuNha { get; set; }
+
+        public DoiTuongSuDung Dtsd { get; set; }
+
+        public MucDichSuDung Mdsd { get; set; }
+
+        public LoaiNha loaiNha { get; set; }
+
+        public List<string> ListDichVu { get; set; }
+
+        public List<string> ListTaiSan { get; set; }
+    }
+}
