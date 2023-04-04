de591e6b0286e4ff9b68c42d132034d81282f39c
Dunno <dungworkit@gmail.com>
2023-04-04 17:12:12 +0700

    x

---
+++ BTL/Models/NhaDichVu.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BTL.Models;
+
+public partial class NhaDichVu
+{
+    public int MaNha { get; set; }
+
+    public int MaDichVu { get; set; }
+
+    public string? GhiChu { get; set; }
+
+    public virtual DichVu MaDichVuNavigation { get; set; } = null!;
+
+    public virtual ThongTinNha MaNhaNavigation { get; set; } = null!;
+}
