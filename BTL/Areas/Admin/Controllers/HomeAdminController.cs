6afa06024de7e97cf5fe59df01d5c46f5a7a3360
Dunno <dungworkit@gmail.com>
2023-04-06 21:21:24 +0700

    Dũng phân công công việc cho Vũ, Hải, Dương, Quân

--- BTL/Areas/Admin/Controllers/HomeAdminController.cs
+++ BTL/Areas/Admin/Controllers/HomeAdminController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BTL.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BTL.Areas.Admin.Controllers
 {
@@ -7,11 +8,97 @@ namespace BTL.Areas.Admin.Controllers
     [Route("admin/homeadmin")]
     public class HomeAdminController : Controller
     {
+
+        QltnContext db = new QltnContext();
+
         [Route("")]
         [Route("index")]
         public IActionResult Index()
         {
             return View();
         }
+
+
+        [Route("ThongTinNha")]
+        public IActionResult ThongTinNha()
+        {
+            var listnha = db.ThongTinNhas.ToList();
+            return View(listnha);
+        }
+
+        #region Văn Vũ 11h tối
+
+        #region ChuNha
+        [Route("ThongTinChuNha")]
+        public IActionResult ThongTinChuNha()
+        {
+            return View();
+        }
+
+        #endregion
+
+        #region NguoiDung
+        [Route("ThongTinNguoiDung")]
+        public IActionResult ThongTinNguoiDung()
+        {
+            return View();
+        }
+        #endregion
+
+        #endregion
+
+        #region Ngọc Quân
+        #region DichVu
+        [Route("ThongTinDichVu")]
+        public IActionResult ThongTinDichVu()
+        {
+            return View();
+        }
+        #endregion
+
+        #region DTSD
+        [Route("ThongTinDTSD")]
+        public IActionResult ThongTinDTSD()
+        {
+            return View();
+        }
+        #endregion
+        #endregion
+
+        #region Dương
+        #region HDN
+        [Route("ThongTinHDN")]
+        public IActionResult ThongTinHDN()
+        {
+            return View();
+        }
+        #endregion
+
+        #region LoaiNha
+        [Route("ThongTinLoaiNha")]
+        public IActionResult ThongTinLoaiNha()
+        {
+            return View();
+        }
+        #endregion
+        #endregion
+
+        #region Hải
+        #region MDSD
+        [Route("ThongTinMDSD")]
+        public IActionResult ThongTinMDSD()
+        {
+            return View();
+        }
+        #endregion
+
+        #region TaiSan
+        [Route("ThongTinTaiSan")]
+        public IActionResult ThongTinTaiSan()
+        {
+            return View();
+        }
+        #endregion
+        #endregion
     }
 }
