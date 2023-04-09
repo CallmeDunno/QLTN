a7c5045b10e122487e4fa1e7576e9b9438dca85d
Dunno <dungworkit@gmail.com>
2023-04-09 22:53:25 +0700

    Dungx commit finallllllllllllllyyyyyyyyyyyyy

---
+++ BTL/Controllers/SearchAPIController.cs
@@ -0,0 +1,19 @@
+﻿using BTL.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BTL.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SearchAPIController : ControllerBase
+    {
+        QltnContext db = new QltnContext();
+
+        [HttpGet("{type}/{dtsd}/{mdsd}")]
+        public IEnumerable<ThongTinNha> getthongtinnhabyaddr(int type, int dtsd, int mdsd)
+        {
+            return db.ThongTinNhas.Where(x => x.MaLoai == type && x.MaDtsd == dtsd && x.MaMdsd == mdsd);
+        }
+    }
+}
